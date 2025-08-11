0573d41b5ebee23fce43a70eb79fdeb5191709eb
huytamlee <huytamlee@gmail.com>
2025-08-11 14:26:10 +0700

    Cloud torage, PDF-Image conversion, Key-Value storage and AI feedback

---
+++ app/components/FileUploader.tsx
@@ -0,0 +1,89 @@
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { formatSize } from "~/lib/utils";
+
+interface FileUploaderProps {
+  onFileSelect?: (file: File | null) => void;
+}
+
+const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      const file = acceptedFiles[0] || null;
+      onFileSelect?.(file);
+    },
+    [onFileSelect]
+  );
+
+  const maxFileSize = 20 * 1024 * 1024; // 20MB in bytes
+
+  const { getRootProps, getInputProps, isDragActive, acceptedFiles } =
+    useDropzone({
+      onDrop,
+      multiple: false,
+      accept: { "application/pdf": [".pdf"] },
+      maxSize: 20 * 1024 * 1024,
+    });
+
+  const file = acceptedFiles[0] || null;
+
+  return (
+    <div className="w-full gradient-border">
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <div className="space-y-4 cursor-pointer">
+          {file ? (
+            <div className="flex justify-between items-center">
+              <div
+                className="uploader-selected-file  w-240"
+                onClick={(e) => e.stopPropagation()}
+              >
+                <img src="/images/pdf.png" alt="pdf" className="size-10" />
+                <div className="flex items-center space-x-3">
+                  <div>
+                    <p className="text-sm text-gray-700 font-medium truncate max-w-xs">
+                      {file.name}
+                    </p>
+                    <p className="text-sm text-gray-500">
+                      {formatSize(file.size)}
+                    </p>
+                  </div>
+                </div>
+                <div className="w-10 h-10 flex items-center justify-center bg-gray-50 rounded-full"></div>
+              </div>
+              <div className="w-10 h-10 flex items-center justify-center bg-gray-50 rounded-full -translate-x-10 z-50">
+                <button
+                  className="p-2 cursor-pointer"
+                  onClick={() => {
+                    onFileSelect?.(null);
+                  }}
+                >
+                  <img
+                    src="/icons/cross.svg"
+                    alt="remove"
+                    className="w-4 h-4"
+                  />
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <div className="mx-auto w-16 h-16 flex items-center justify-center mb-2">
+                <img src="/icons/info.svg" alt="upload" className="size-20" />
+              </div>
+              <p className="text-lg text-gray-500">
+                <span className="font-semibold">Click to upload</span> or drag
+                and drop
+              </p>
+              <p className="text-lg text-gray-500">
+                PDF (max {formatSize(maxFileSize)})
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FileUploader;
