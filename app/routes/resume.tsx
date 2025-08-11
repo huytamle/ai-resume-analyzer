dc5a0b80489aae442ed942ee1a986354997b52c9
huytamlee <huytamlee@gmail.com>
2025-08-11 14:39:23 +0700

    Fixing PDF cannot be converted error

---
+++ app/routes/resume.tsx
@@ -0,0 +1,22 @@
+import { Link, useParams } from "react-router";
+
+export const meta = () => [
+  { title: "Resumind | Review" },
+  { name: "description", content: "Detailed overview of your resume" },
+];
+
+const Resume = () => {
+  const { id } = useParams();
+
+  return (
+    <main className="!pt-0">
+      <nav className="resume-nav">
+        <Link to="/" className="back-button">
+          <img src="/icons/back.svg" alt="logo" />
+        </Link>
+      </nav>
+    </main>
+  );
+};
+
+export default Resume;
